5cc570eee0042d310e5bf3ed7c4f783f00470322
urchadezaratiana <Riazcass74>
2024-07-17 11:18:59 +0200

    more token processor to data_processor.py

--- modules/data_processor.py
+++ modules/data_processor.py
@@ -97,7 +97,8 @@ class GrapherData(object):
             ent_to_id, id_to_ent = self.create_mapping(entity_types)
             rel_to_id, id_to_rel = self.create_mapping(relation_types)
 
-        batch = [self.preprocess_spans(b["tokenized_text"], b["entities"], b["relations"], ent_to_id[i]) for i, b in
+        batch = [self.preprocess_spans(b["tokenized_text"], b["entities"], b["relations"],
+                                       ent_to_id if not isinstance(ent_to_id, list) else ent_to_id[i]) for i, b in
                  enumerate(batch_list)]
 
         return self.create_batch_dict(batch, ent_to_id, id_to_ent, rel_to_id, id_to_rel)
@@ -135,3 +136,129 @@ class GrapherData(object):
     def create_dataloader(self, data, entity_types=None, relation_types=None, **kwargs) -> DataLoader:
         return DataLoader(data, collate_fn=lambda x: self.collate_fn(x, entity_types, relation_types), **kwargs)
 
+
+class TokenPromptProcessorTR:
+    def __init__(self, entity_token, relation_token, sep_token):
+        self.entity_token = entity_token
+        self.sep_token = sep_token
+        self.relation_token = relation_token
+
+    def process(self, x, token_rep_layer, mode):
+        if mode == "train":
+            return self._process_train(x, token_rep_layer)
+        elif mode == "eval":
+            return self._process_eval(x, token_rep_layer)
+        else:
+            raise ValueError("Invalid mode specified. Choose 'train' or 'eval'.")
+
+    def _process_train(self, x, token_rep_layer):
+
+        device = next(token_rep_layer.parameters()).device
+
+        new_length = x["seq_length"].clone()
+        new_tokens = []
+        all_len_prompt = []
+        num_classes_all = []
+        num_relations_all = []
+
+        for i in range(len(x["tokens"])):
+            all_types_i = list(x["ent_to_id"][i].keys())
+            all_relations_i = list(x["rel_to_id"][i].keys())
+            entity_prompt = []
+            relation_prompt = []
+            num_classes_all.append(len(all_types_i))
+            num_relations_all.append(len(all_relations_i))
+
+            for entity_type in all_types_i:
+                entity_prompt.append(self.entity_token)
+                entity_prompt.append(entity_type)
+            entity_prompt.append(self.sep_token)
+
+            for relation_type in all_relations_i:
+                relation_prompt.append(self.relation_token)
+                relation_prompt.append(relation_type)
+            relation_prompt.append(self.sep_token)
+
+            combined_prompt = entity_prompt + relation_prompt
+            tokens_p = combined_prompt + x["tokens"][i]
+            new_length[i] += len(combined_prompt)
+            new_tokens.append(tokens_p)
+            all_len_prompt.append(len(combined_prompt))
+
+        max_num_classes = max(num_classes_all)
+        entity_type_pos = torch.arange(max_num_classes).unsqueeze(0).expand(len(num_classes_all), -1).to(device)
+        entity_type_mask = entity_type_pos < torch.tensor(num_classes_all).unsqueeze(-1).to(device)
+
+        max_num_relations = max(num_relations_all)
+        relation_type_pos = torch.arange(max_num_relations).unsqueeze(0).expand(len(num_relations_all), -1).to(device)
+        relation_type_mask = relation_type_pos < torch.tensor(num_relations_all).unsqueeze(-1).to(device)
+
+        bert_output = token_rep_layer(new_tokens, new_length)
+        word_rep_w_prompt = bert_output["embeddings"]
+        mask_w_prompt = bert_output["mask"]
+
+        word_rep = []
+        mask = []
+        entity_type_rep = []
+        relation_type_rep = []
+
+        for i in range(len(x["tokens"])):
+            prompt_entity_length = all_len_prompt[i]
+            entity_len = 2 * len(list(x["ent_to_id"][i].keys())) + 1
+            relation_len = 2 * len(list(x["rel_to_id"][i].keys())) + 1
+
+            word_rep.append(word_rep_w_prompt[i, prompt_entity_length:new_length[i]])
+            mask.append(mask_w_prompt[i, prompt_entity_length:new_length[i]])
+
+            entity_rep = word_rep_w_prompt[i, :entity_len - 1]
+            entity_rep = entity_rep[0::2]
+            entity_type_rep.append(entity_rep)
+
+            relation_rep = word_rep_w_prompt[i, entity_len:entity_len + relation_len - 1]
+            relation_rep = relation_rep[0::2]
+            relation_type_rep.append(relation_rep)
+
+        word_rep = pad_sequence(word_rep, batch_first=True)
+        mask = pad_sequence(mask, batch_first=True)
+        entity_type_rep = pad_sequence(entity_type_rep, batch_first=True)
+        relation_type_rep = pad_sequence(relation_type_rep, batch_first=True)
+
+        return word_rep, mask, entity_type_rep, entity_type_mask, relation_type_rep, relation_type_mask
+
+    def _process_eval(self, x, token_rep_layer):
+        all_types = list(x["ent_to_id"].keys())
+        all_relations = list(x["rel_to_id"].keys())
+        entity_prompt = []
+        relation_prompt = []
+
+        for entity_type in all_types:
+            entity_prompt.append(self.entity_token)
+            entity_prompt.append(entity_type)
+        entity_prompt.append(self.sep_token)
+
+        for relation_type in all_relations:
+            relation_prompt.append(self.relation_token)
+            relation_prompt.append(relation_type)
+        relation_prompt.append(self.sep_token)
+
+        combined_prompt = entity_prompt + relation_prompt
+        prompt_entity_length = len(combined_prompt)
+        tokens_p = [combined_prompt + tokens for tokens in x["tokens"]]
+        seq_length_p = x["seq_length"] + prompt_entity_length
+
+        # Converting tokens_p to a format suitable for token_rep_layer
+        out = token_rep_layer(tokens_p, seq_length_p)
+
+        word_rep_w_prompt = out["embeddings"]
+        mask_w_prompt = out["mask"]
+
+        word_rep = word_rep_w_prompt[:, prompt_entity_length:, :]
+        mask = mask_w_prompt[:, prompt_entity_length:]
+
+        entity_type_rep = word_rep_w_prompt[:, :len(entity_prompt) - 1, :]
+        entity_type_rep = entity_type_rep[:, 0::2, :]
+
+        relation_type_rep = word_rep_w_prompt[:, len(entity_prompt):prompt_entity_length - 1, :]
+        relation_type_rep = relation_type_rep[:, 0::2, :]
+
+        return word_rep, mask, entity_type_rep, relation_type_rep
